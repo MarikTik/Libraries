0575f3c26eb3b24a11fbeafe8537316fee23df89
Mark Tikhonov <mtik.philosopher@gmail.com>
2023-03-10 22:04:47 -0800

    Began working on a task library implementation, for now supplied the basic interface of a task

---
+++ include/Tasks/Task.h
@@ -0,0 +1,11 @@
+#ifndef TASK_H_
+#define TASK_H_
+
+struct Task{
+   virtual void initialize() = 0;
+   virtual void execute() = 0;
+   virtual void end() = 0;
+   virtual bool isFinished() const = 0;
+};
+
+#endif
